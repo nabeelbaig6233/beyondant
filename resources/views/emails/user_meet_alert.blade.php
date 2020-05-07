6abd18c3a7d99f039d159f7d8ddfbdbd6b7f8185
amirali2019 <amir.ali@tachnado.co>
2020-05-08 01:33:24 +0500

    User Alert Change

--- resources/views/emails/user_meet_alert.blade.php
+++ resources/views/emails/user_meet_alert.blade.php
@@ -10,6 +10,7 @@
     <h2>Hello {{ucwords($first_name)}},</h2>
     <p>Please be advised that your vCard has recently been downloaded. When possible log into your Beyondant account to view additional details.</p>
     <p>Login: <a href="https://beyondant.com/">www.beyondant.com</a></p>
+    <p>For the best experience viewing your contacts please log into your account on a desktop computer. </p>
     <p>Thank you!</p>
     @slot('footer')
         @component('mail::footer')
