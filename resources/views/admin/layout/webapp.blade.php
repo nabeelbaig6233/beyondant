8b5feb872a4cae4ec619c79e18a245895ee4ea07
amirali2019 <amir.ali@tachnado.co>
2020-05-08 21:44:27 +0500

    My Public Profile

--- resources/views/admin/layout/webapp.blade.php
+++ resources/views/admin/layout/webapp.blade.php
@@ -122,7 +122,7 @@
                         <div class="menu_section">
                             <h3>Profile</h3>
                             <ul class="nav side-menu">
-                                <li><a><i class="fa fa-eye"></i> {{auth()->user()->role_id===5?"Company Profile":"My Profile"}} <span class="fa fa-chevron-down"></span></a>
+                                <li><a><i class="fa fa-eye"></i> {{auth()->user()->role_id===5?"Company Profile":"My Public Profile"}} <span class="fa fa-chevron-down"></span></a>
                                     <ul class="nav child_menu">
                                         @if(in_array('viewContact',\Request::get('permission')))
                                             <li><a href="{{route('pro',auth()->user()->id)}}" target="_blank">View Profile</a></li>
