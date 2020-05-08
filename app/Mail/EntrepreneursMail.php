aa609a0750f8b2eeeff4324c2e2446895f4371f8
amirali2019 <amir.ali@tachnado.co>
2020-05-09 02:57:22 +0500

    Mail Subjects

--- app/Mail/EntrepreneursMail.php
+++ app/Mail/EntrepreneursMail.php
@@ -30,6 +30,6 @@ class EntrepreneursMail extends Mailable
      */
     public function build()
     {
-        return $this->markdown('emails.entrepreneurs')->with("entrepreneursMail",$this->entrepreneursMail)->from("Notifications@Beyondant.com");
+        return $this->markdown('emails.entrepreneurs')->with("entrepreneursMail",$this->entrepreneursMail)->from("Notifications@Beyondant.com")->subject("From Beyondant Affiliate Channel");
     }
 }
