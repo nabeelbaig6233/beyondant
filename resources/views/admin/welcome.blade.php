5c941985b4c5c5fef47605ccf2c3d60ef8ae50c9
amirali2019 <amir.ali@tachnado.co>
2020-05-22 02:52:40 +0500

    Date Filters Google Data

--- resources/views/admin/welcome.blade.php
+++ resources/views/admin/welcome.blade.php
@@ -1,4 +1,7 @@
 @extends('admin.layout.webapp')
+@section('page_css')
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+@endsection
 @section('content')
   <div class="right_col" role="main">
     <div class="row col-lg-12 mb-3" style="background: linear-gradient(to top right, #3366cc -8%, #66ff99 109%);border-radius: 10px;padding: 20px">
@@ -46,8 +49,24 @@
                                   <span class="sr-only">Loading...</span>
                               </div>
                           </div>
-
-                          <canvas id="analytics_chart" width="400" height="140"></canvas>
+                        <div class="card">
+                            <div class="card-header">
+                                <div class="row">
+                                    <div class="col-3">
+                                        <button class="btn btn-block btn-danger" id="select_date" style="background:linear-gradient(90deg, rgba(162,18,5,0.9640231092436975) 22%, rgba(121,9,75,1) 94%);border-radius: 10px;padding: 10px;border: none;">Select Date</button>
+                                    </div>
+                                    <div class="col-8" id="start_end_date" style="display: none">
+                                        <div class="row p-3">
+                                            <h4 class="text-dark font-weight-bold ml-3 mr-2">Start Date: </h4><span class="text-danger font-weight-bold ml-2"></span>
+                                            <h4 class="text-dark font-weight-bold ml-3 mr-2">End Date: </h4><span class="text-danger font-weight-bold ml-2"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <canvas id="analytics_chart" width="400" height="140"></canvas>
+                            </div>
+                        </div>
                       </div>
                   </div>
           @endif
@@ -59,8 +78,12 @@
 @section('page_js')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+
     <script>
         $(document).ready(function () {
+            //Chart Initialization
             var chart=new Chart($("#analytics_chart"),{
                 type:'line',
                 data:{
@@ -91,7 +114,20 @@
                     }
 
             });
-            $.get('{{route('google-analytics',auth()->user()->id)}}',function (data) {
+            //Chart Initialization
+
+            //Chart TimeSeries Formatter
+            const timeSFormatter=(dataLength)=>{
+                if(dataLength<15){return 'day';}
+                else if(dataLength>55){return 'month';}
+                else{return 'week';}
+            };
+            //Chart TimeSeries Formatter
+
+            //Updating Chart
+
+            const chartUpdate=(data)=>{
+                $("#loading_graph").css({display:'block'});
                 var data=JSON.parse(data);
                 var labels=[];
                 var values=[];
@@ -102,18 +138,61 @@
                     values.push(views);
                     total+=views;
                 });
-
                 chart.data.labels=labels;
                 chart.data.datasets[0].data=values;
                 chart.options.scales.yAxes[0].ticks.fontSize = 12 ;
-                chart.options.scales.xAxes[0].time.unit=data.length<15?'day':'week';
+                chart.options.scales.xAxes[0].time.unit=timeSFormatter(data.length);
                 $("#loading_graph").css({display:'none'});
-                $("#total_views").text(total);
                 chart.update();
+                return total;
+            }
+
+            //Updating Chart
+
+            //Getting Initial Data
+            $.get('{{route('google-analytics',auth()->user()->id)}}',function (data) {
+                var total=chartUpdate(data);
+                $("#total_views").text(total);
                 }).catch(function (error) {
                 console.log(error);
             });
+            //Getting Initial Data
 
+            //Getting FIlter Data
+            async function filterAnalytics(startDate,endDate){
+                $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': `{{csrf_token()}}`
+                    }
+                });
+
+                 const result=await $.ajax({
+                   url:`{{route('google-filter-analytics')}}`,
+                   type:'POST',
+                   data:{'startDate':startDate,'endDate':endDate}
+                });
+                return result;
+            }
+            //Getting FIlter Data
+
+            //DatePicker Starts
+            $('#select_date').daterangepicker({
+                maxDate:moment().subtract(2,'days'),
+                minDate:moment().subtract(4,'months').subtract(2,'days')
+            },function(start,end){
+                var display_format='MM/DD/YYYY';
+                var query_format='YYYY-MM-DD';
+                $("#start_end_date span:eq(0)").text(start.format(display_format));
+                $("#start_end_date span:eq(1)").text(end.format(display_format));
+                $("#start_end_date").css({display: 'block'});
+                var getFA=filterAnalytics(start.format(query_format),end.format(query_format));
+                getFA.then((data)=>{
+                    chartUpdate(data);
+                }).catch((err)=>{
+                    console.log(err);
+                });
+            });
+            //DatePicker Ends
 
         })
     </script>
