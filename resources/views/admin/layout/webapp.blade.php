9b9d21bf22f4432de8b9b5dbf567c8719f2341a7
amirali2019 <amir.ali@tachnado.co>
2020-06-05 00:16:27 +0500

    All profiles to Profile Search

--- resources/views/admin/layout/webapp.blade.php
+++ resources/views/admin/layout/webapp.blade.php
@@ -95,9 +95,9 @@
                         <div class="menu_section">
                             <h3>Users</h3>
                             <ul class="nav side-menu">
-                                <li><a><i class="fa fa-users"></i> All Profiles <span class="fa fa-chevron-down"></span></a>
+                                <li><a><i class="fa fa-users"></i>Profile Search<span class="fa fa-chevron-down"></span></a>
                                     <ul class="nav child_menu">
-                                            <li><a href="{{route('allProfiles')}}">Profiles</a></li>
+                                            <li><a href="{{route('allProfiles')}}">Search</a></li>
                                     </ul>
                                 </li>
 
